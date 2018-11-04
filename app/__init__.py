d8f17455db792cb799741e451ca3fee1f436a13b
Reed Molbak <reed@benchling.com>
2018-11-04 11:40:20 -0800

    Add shell context function

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from app import models
 from app import routes
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': models.User}
 
 if __name__ == '__main__':
     app.run()
