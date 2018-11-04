85bc6b4b4ef3fccf354f97ea705d799c22e8288f
Reed Molbak <reed@benchling.com>
2018-11-04 09:42:18 -0800

    Disable SQLALCHEMY_TRACK_MODIFICATIONS

--- kvasir/app.py
+++ kvasir/app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
