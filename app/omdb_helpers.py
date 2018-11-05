bd24294e577408f5f738ac28ce099326600f0c00
Naomi Jacobs <naomiajacobs@gmail.com>
2018-11-04 21:15:33 -0800

    Add omdb helper and route

---
+++ app/omdb_helpers.py
@@ -0,0 +1,7 @@
+import omdb
+import os
+
+omdb.set_default('apikey', os.environ['OMDB_API_KEY'])
+
+def search_movies(search_term):
+    return omdb.search(search_term)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,16 @@
+import json
+
 from app import app
+from flask import request
+from .omdb_helpers import search_movies
 
 @app.route('/')
 def hello():
     return 'Hello world!'
+
+@app.route('/search-movies')
+def search_omdb_movies():
+    searchword = request.args.get('search', '')
+    return json.dumps({
+        'searchResults': search_movies(searchword),
+    })
