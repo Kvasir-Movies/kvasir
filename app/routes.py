21119891f7dbf53d6c0a49c6598ca35b28feb70a
Naomi Jacobs <naomiajacobs@gmail.com>
2018-11-04 21:30:37 -0800

    Remove redundant home route

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,10 @@ from app import app
 from app.controllers import HomeController, OMDBSearchController
 
 @app.route('/')
-def hello():
-    return 'Hello world!'
+def home():
+    return HomeController().handle()
 
 @app.route('/search-movies')
 def search_omdb_movies():
     return OMDBSearchController().handle()
-
-@app.route('/')
-def home():
-    return HomeController().handle()
 
