2deecb69e69fdf81eaae88f36425f688d183d1bb
Reed Molbak <reed@benchling.com>
2018-11-05 22:04:31 -0800

    Get basic routing working

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // @ts-ignore
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class AppRouter extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/login' component={LoginPage} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+class HomePage extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Kvasir Movies</h1>
           <p>
-            Edit <code>client-src/App.js</code> and save to reload.
+            Find 🎬 with 👫 :D
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <a href='/login'>Log In</a>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+class LoginPage extends Component {
+  render() {
+    return <div>Log in stuff will go here</div>
+  }
+}
+
+export default AppRouter;
