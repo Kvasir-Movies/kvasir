3fe6603fb7ace674e36d6571c32759826f456634
Reed Molbak <reed@benchling.com>
2018-11-06 21:41:54 -0800

    Test catch_all router for flask

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app.controllers import HomeController, LoginController, LoginPageController
 from app.models import User
 from app.session_util import login_required
 
-from flask import jsonify
+from flask import jsonify, render_template
 
 
 @app.route('/current-user')
@@ -12,12 +12,16 @@ def get_current_user():
     user_dict = {'email': user.email} if user else None
     return jsonify(user=user_dict)
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('index.html')
 
 # Routes for serving html and js (deprecated)
-@app.route('/')
-@login_required
-def home():
-    return HomeController().handle()
+# @app.route('/')
+# @login_required
+# def home():
+#     return HomeController().handle()
 
 @app.route('/search-movies')
 @login_required
