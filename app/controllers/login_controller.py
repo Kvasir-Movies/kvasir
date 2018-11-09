7b809129a6bf0ad0644a8ed72200c4a6cd4a821b
Naomi Jacobs <naomiajacobs@gmail.com>
2018-11-08 21:25:00 -0800

    Add redirect logic for logging in successfully

--- app/controllers/login_controller.py
+++ app/controllers/login_controller.py
@@ -1,27 +1,28 @@
+import json
+
+from flask import jsonify, request, redirect, session, url_for
+
 from app.models import User
-
-from flask import jsonify, request, redirect, url_for
-
 from app.password_util import hash_password, validate_password
 from app.session_util import is_user_logged_in, create_session, delete_session
 
 class LoginController():
     def handle(self):
-        req_data = request.form
-        email = req_data['email']
-        password = req_data['password']
+        data = json.loads(request.data)
+        email = data['email']
+        password = data['password']
 
         user = User.query.filter(User.email == email).one_or_none()
 
         if not user:
             delete_session()
-            return jsonify(success=False)
+            return '', 401
 
         password_hash = user.password_hash
         is_password_valid = validate_password(password, password_hash)
         if (is_password_valid):
             create_session(email)
-            return jsonify(success=True)
+            return jsonify(is_session_active=True, email=session['email'])
         else:
             delete_session()
-            return jsonify(success=False)
+            return '', 401
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, FormEvent, ChangeEvent } from 'react';
+import SessionInfo from '../types';
+import { Redirect } from 'react-router';
 
 class LoginPage extends Component {
+    readonly state: {
+        email: string | null,
+        password: string | null,
+        sessionInfo: SessionInfo | null,
+    } = {
+        email: null,
+        password: null,
+        sessionInfo: null,
+    };
+
+    handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit = (event: any) => {
+        event.preventDefault();
+        fetch('/login', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            }
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error(`Login failed: ${response.status} ${response.statusText || ''}`);
+                }
+            })
+            .then((data: {sessionInfo: SessionInfo}) => {
+                this.setState({sessionInfo: data});
+            })
+            .catch((errorMessage) => alert(errorMessage));
+    }
+
     render() {
-        // TODO: Redirect home after submission
         return(
-        <form method="POST">
-            Email: <input type="text" name="email" /><br/>
-            Password: <input type="password" name="password" /><br/>
-            <input type="submit" value="Submit" /><br/>
-        </form>
+            this.state.sessionInfo ?
+            (<Redirect to="/" />) :
+            (<form onSubmit={this.handleSubmit}>
+                Email: <input type="text" name="email" onChange={this.handleInputChange} /><br/>
+                Password: <input type="password" name="password" onChange={this.handleInputChange} /><br/>
+                <input type="submit" value="Submit" /><br/>
+            </form>)
         );
     }
 }
