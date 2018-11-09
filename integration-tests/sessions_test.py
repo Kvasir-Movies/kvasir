f6c7838b9733720534151e9b55dcbe11b5732d41
Naomi Jacobs <naomiajacobs@gmail.com>
2018-11-09 09:02:25 -0800

    Make test runnable

--- integration-tests/sessions_test.py
+++ integration-tests/sessions_test.py
@@ -10,11 +10,17 @@ class SignupTest(unittest.TestCase):
         driver = webdriver.Chrome()
         driver.get(LOCAL_HOST)
 
-        assert driver.title == "Kvasir Movies"
+        self.assertEqual(driver.title, "Kvasir Movies")
         driver.find_element_by_class_name("signup").click()
 
         email_input = driver.find_element_by_name('email')
-        assert email_input
+        email_input.clear()
+        email_input.send_keys('naomi@jacobs.com')
         password_input = driver.find_element_by_name('password')
-        assert password_input
+        password_input.clear()
+        password_input.send_keys('super secret')
         driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
