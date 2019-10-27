1e7eac0cb35f99f85b8d0ac87461117d77fe3a46
Naomi Jacobs <naomiajacobs@gmail.com>
2019-10-27 00:45:01 -0700

    Update Watch page to fix bugs and use shared MoveList

--- src/components/MovieRecommender.tsx
+++ src/components/MovieRecommender.tsx
@@ -13,35 +13,57 @@ const MovieRecommender = ({
 }): JSX.Element => {
   const [recommendedMovies, setRecommendedMovies] = useState<Array<Movie>>([]);
   const [selectedEmails, setSelectedEmails] = useState<Array<string>>([]);
+  const [fetchCompleted, setFetchCompleted] = useState(false);
+
+  const resetEmails = (emails: Array<string>): void => {
+    // clear out recommended movies since selected emails has changed
+    setRecommendedMovies([]);
+    setFetchCompleted(false);
+    setSelectedEmails(emails);
+  };
 
   const handleFetchMovieRecommendation = async () => {
+    setFetchCompleted(false);
+    setRecommendedMovies([]);
     const recommendedMovies = await getRecommendation(selectedEmails.join(","));
+    setFetchCompleted(true);
     setRecommendedMovies(recommendedMovies);
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <div className="flex-fill">
-        <UserPicker
-          sessionUser={sessionUser}
-          selectedEmails={selectedEmails}
-          setSelectedEmails={setSelectedEmails}
-        />
-        <Button
-          onClick={handleFetchMovieRecommendation}
-          primary
-          style={{ marginLeft: "0.5em" }}
-        >
-          Find Movies
-        </Button>
-      </div>
+    <>
+      <Header as="h3">Who's watching today?</Header>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ display: "flex" }}>
+          <UserPicker
+            sessionUser={sessionUser}
+            selectedEmails={selectedEmails}
+            setSelectedEmails={resetEmails}
+          />
+          <Button
+            onClick={handleFetchMovieRecommendation}
+            primary
+            disabled={!selectedEmails.length}
+          >
+            Find Movies
+          </Button>
+        </div>
 
-      <Segment.Group>
-        {recommendedMovies.map((movie: Movie, index: number) => (
-          <Segment key={movie.externalMovieId}>{movie.title}</Segment>
-        ))}
-      </Segment.Group>
-    </div>
+        <Header as="h3">Movies in Common</Header>
+        {selectedEmails.length === 0 &&
+          "Search for friends to find movies you all want to watch."}
+        {fetchCompleted &&
+          selectedEmails.length > 0 &&
+          (recommendedMovies.length ? (
+            <MovieList
+              movies={recommendedMovies}
+              changeMoviePreference={() => {}}
+            />
+          ) : (
+            "No common movies found - try adding more movies to your lists to find ones in common"
+          ))}
+      </div>
+    </>
   );
 };
 
